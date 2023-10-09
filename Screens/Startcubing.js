376c95692c364a3aff2da8f749e49a6cd8d9bfe2
aliense1 <111488791+aliense1@users.noreply.github.com>
2023-10-09 18:41:31 +0530

    Add files via upload

---
+++ Screens/Startcubing.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+export default class StartCubing extends React.Component{
+
+  render(){
+    return (
+     <View>
+      <Text>HI</Text>
+     </View>
+    )
+  }
+}
